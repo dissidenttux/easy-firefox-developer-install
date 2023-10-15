30e7204ce6c622ca955cd24e67d29b2dfa8c758e
Michael "Cal" Krug <cal.krug@murena.io>
2023-10-15 02:33:15 -0500

    Add: Mass changes to README & install.sh into working state

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+
+cd /opt/
 # extract downloaded tar
-tar xvzf /opt/firefox-dev-current.tar.bz2
+sudo tar -xf /opt/firefox-dev-current.tar.bz2 
 
 # move the firefox to the optional software folder
 # mv ~/Download/easy-firefox-developer-install/firefox-dev-current.tar.bz2 /opt/
 
 # remove downloaded tar
-rm /opt/firefox-dev-current.tar.bz2
+rm firefox-dev-current.tar.bz2
 # renames the extracted file to "firefox-dev" for interchangability with normal firefox
-cp /opt/firefox /opt/firefox-dev
+mv /opt/firefox /opt/firefox-dev
 
 # copy the launch script to the user-made script folder
 # cp ~/Download/easy-firefox-developer-install/firefox-dev /usr/local/bin/
