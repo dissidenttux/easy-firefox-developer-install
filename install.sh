6273e98e5a77582d381d82df1ed94b3362a3d2ce
Michael "Cal" Krug <cal.krug@murena.io>
2023-10-15 01:09:15 -0500

    Add: install script change firefox file move method

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # get latest firefox-dev file into the optional software directory
-wget -P /opt/ wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -O firefox-dev-current.tar.bz2
+wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -O firefox-dev-current.tar.bz2
+# move downloaded firefox to optional software directory
+mv ~/Downloads/easy-firefox-developer-install/firefox-dev-current.tar.bz2 /opt/
 # extract downloaded tar
-tar –xvzf /opt/firefox-dev-current.tar.bz2
+tar xvzf /opt/firefox-dev-current.tar.bz2
 # remove downloaded tar
 rm /opt/firefox-dev-current.tar.bz2
 # renames the extracted file to "firefox-dev" for interchangability with normal firefox
