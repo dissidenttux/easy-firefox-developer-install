d550e8dbb7c0e1fafb94a8bf18de3f2418788b43
Michael "Cal" Krug <cal.krug@murena.io>
2023-10-15 00:55:18 -0500

    Add: Changes to install script wget

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # get latest firefox-dev file into the optional software directory
-wget -P /opt/ https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US
+wget -P /opt/ wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -O firefox-dev-current.tar.bz2
+# extract downloaded tar
+tar –xvzf /opt/firefox-dev-current.tar.bz2
+# remove downloaded tar
+rm /opt/firefox-dev-current.tar.bz2
 # renames the extracted file to "firefox-dev" for interchangability with normal firefox
 cp /opt/firefox /opt/firefox-dev
 # move the launch script to the user-made script folder
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # removes "firefox-dev" from optional software folder
-rm /opt/firefox-dev
+rm -r /opt/firefox-dev
 # move the launch script to the user-made script folder
 rm /usr/local/bin/firefox-dev
