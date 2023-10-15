4b2c026068eff643c0740e01e35dbc0834a91a35
Michael "Cal" Krug <cal.krug@murena.io>
2023-10-15 02:45:59 -0500

    Add: install script cleanup

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-
+# navigate to the optional software directory
 cd /opt/
 # extract downloaded tar
 sudo tar -xf /opt/firefox-dev-current.tar.bz2 
-
-# move the firefox to the optional software folder
-# mv ~/Download/easy-firefox-developer-install/firefox-dev-current.tar.bz2 /opt/
-
+# NON-WORKING move the firefox to the optional software folder
+# NON-WORKING mv ~/Download/easy-firefox-developer-install/firefox-dev-current.tar.bz2 /opt/
 # remove downloaded tar
 rm firefox-dev-current.tar.bz2
 # renames the extracted file to "firefox-dev" for interchangability with normal firefox
 mv /opt/firefox /opt/firefox-dev
-
-# copy the launch script to the user-made script folder
-# cp ~/Download/easy-firefox-developer-install/firefox-dev /usr/local/bin/
-
+# NON-WORKING copy the launch script to the user-made script folder
+# NON-WORKING cp ~/Download/easy-firefox-developer-install/firefox-dev /usr/local/bin/
 # make the launch script executable
 chmod a+x /usr/local/bin/firefox-dev
